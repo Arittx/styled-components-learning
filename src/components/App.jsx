51cb66524cf2714f127afccc9ab004c52deddf03
Aracelly Peña Gonzalez <ara2896@gmail.com>
2023-05-04 11:53:31 -0500

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import LightTheme from "../themes/light.jsx";
+import DarkTheme from "../themes/dark.jsx";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: ${(p) => p.theme.bodyBackgroundColor}; 
+    min-height: 100vh;
+    margin: 0;
+    color: ${(p) => p.theme.bodyFontColor};
+    font-family: 'Kaushan Script';
+  }
+`;
+
+function App() {
+  const [theme, setTheme] = useState(LightTheme);
+
+  return (
+    <ThemeProvider
+      theme={{
+        ...theme,
+        setTheme: () => {
+          setTheme((s) => (s.id === "light" ? DarkTheme : LightTheme));
+        },
+      }}
+    >
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
